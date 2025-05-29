38631dc6c225dda423156bd90e7650e66a29ba2e
pRom3o <alameleromeo1@gmail.com>
2025-05-29 23:44:02 +0100

    search modal 99% done

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -12,32 +12,39 @@ const animename = ref('')
 
 // Create a reactive variable to hold the fetched anime data
 const animeresult = ref([])
-
+// create a variable for loading state
+const loading = ref(false)
 // function to fetch  data from API
 async function fetchanime() {
-  // fetch anime info from jinka API
-  try {
-    const response = await fetch(`https://api.jikan.moe/v4/anime?q=${animename.value}`)
-
-    // to check if request was successful
-    if (!response.ok) {
-      throw new Error('Unsuccessful, Try again')
+  // loading state
+  loading.value = true
+  // setTimeout for loading screen
+  setTimeout(async () => {
+    // fetch anime info from jinka API
+    try {
+      const response = await fetch(`https://api.jikan.moe/v4/anime?q=${animename.value}`)
+      // to check if request was successful
+      if (!response.ok) {
+        throw new Error('Unsuccessful, Try again')
+      }
+      // convert response to json format and store
+      const data = await response.json()
+      // store data in ref array
+      animeresult.value = data.data
+      // log to console to check value
+      console.log(animeresult.value)
+    } catch (error) {
+      // Handle any errors if the request fails
+      console.error('Error fetching anime:', error)
+    } finally {
+      loading.value = false
     }
-    // convert response to json format and store
-    const data = await response.json()
-    // store data in ref array
-    animeresult.value = data.data
-    // log to console to check value
-    console.log(animeresult.value)
-  } catch (error) {
-    // Handle any errors if the request fails
-    console.error('Error fetching anime:', error)
-  }
+  }, 500)
 }
 </script>
 
 <template>
-  <div class="w-full h-full flex justify-center px-4" v-show="searchbar">
+  <div class="w-full h-full flex justify-center px-4 backdrop-blur-xs" v-show="searchbar">
     <div
       class="md:mt-40 mt-30 mb-20 md:w-[30%] md:h-[50%] h-[70%] bg-[#181818] rounded-4xl flex flex-col items-center justify-between p-6"
     >
@@ -107,10 +114,53 @@ async function fetchanime() {
         class="h-[85%] w-full space-y-3 flex flex-col items-center text-gray-400 overflow-y-auto scroll-hidden"
         v-else
       >
+        <div class="h-full w-full flex items-center justify-center" v-if="loading">
+          <p>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+              <g
+                fill="none"
+                stroke="#fff"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+              >
+                <path stroke-dasharray="16" stroke-dashoffset="16" d="M12 3c4.97 0 9 4.03 9 9">
+                  <animate
+                    fill="freeze"
+                    attributeName="stroke-dashoffset"
+                    dur="0.225s"
+                    values="16;0"
+                  />
+                  <animateTransform
+                    attributeName="transform"
+                    dur="1.125s"
+                    repeatCount="indefinite"
+                    type="rotate"
+                    values="0 12 12;360 12 12"
+                  />
+                </path>
+                <path
+                  stroke-dasharray="64"
+                  stroke-dashoffset="64"
+                  stroke-opacity="0.3"
+                  d="M12 3c4.97 0 9 4.03 9 9c0 4.97 -4.03 9 -9 9c-4.97 0 -9 -4.03 -9 -9c0 -4.97 4.03 -9 9 -9Z"
+                >
+                  <animate
+                    fill="freeze"
+                    attributeName="stroke-dashoffset"
+                    dur="0.9s"
+                    values="64;0"
+                  />
+                </path>
+              </g>
+            </svg>
+          </p>
+        </div>
         <div
-          class="w-full max-h-[35%] p-3 flex items-center md:space-x-0 space-x-3 text-white border-b border-[#333333]"
+          class="w-full max-h-[35%] py-2 flex items-center md:space-x-0 space-x-3 text-white border-b border-[#333333]"
           v-for="anime in animeresult"
           :key="anime.mal_id"
+          v-else
         >
           <div class="w-[20%] h-full">
             <img :src="anime.images.jpg.image_url" :alt="anime.title[0]" class="h-full" />
@@ -118,28 +168,39 @@ async function fetchanime() {
           <div
             class="w-[70%] max-h-full flex flex-col items-start text-start space-y-1 overflow-y-auto scroll-hidden"
           >
-            <h2 class="font-bold">{{ anime.title_english }}</h2>
+            <h2 class="font-bold text-sm md:text-base">{{ anime.title_english }}</h2>
             <p class="text-xs">Episodes - {{ anime.episodes }}</p>
-            <p class="text-xs flex items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="12"
-                height="12"
-                viewBox="0 0 24 24"
-                class="mr-1"
-              >
-                <path
-                  fill="none"
-                  stroke="#fff"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="1.5"
-                  d="m13.728 3.444l1.76 3.549c.24.494.88.968 1.42 1.058l3.189.535c2.04.343 2.52 1.835 1.05 3.307l-2.48 2.5c-.42.423-.65 1.24-.52 1.825l.71 3.095c.56 2.45-.73 3.397-2.88 2.117l-2.99-1.785c-.54-.322-1.43-.322-1.98 0L8.019 21.43c-2.14 1.28-3.44.322-2.88-2.117l.71-3.095c.13-.585-.1-1.402-.52-1.825l-2.48-2.5C1.39 10.42 1.86 8.929 3.899 8.586l3.19-.535c.53-.09 1.17-.564 1.41-1.058l1.76-3.549c.96-1.925 2.52-1.925 3.47 0"
-                  color="#fff"
-                />
-              </svg>
-              {{ anime.score }} - {{ anime.type }} - {{ anime.year }} - {{ anime.status }}
-            </p>
+            <div class="text-xs w-full flex items-center justify-between">
+              <p class="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="12"
+                  height="12"
+                  viewBox="0 0 24 24"
+                  class="mr-1"
+                >
+                  <path
+                    fill="none"
+                    stroke="#fff"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="1.5"
+                    d="m13.728 3.444l1.76 3.549c.24.494.88.968 1.42 1.058l3.189.535c2.04.343 2.52 1.835 1.05 3.307l-2.48 2.5c-.42.423-.65 1.24-.52 1.825l.71 3.095c.56 2.45-.73 3.397-2.88 2.117l-2.99-1.785c-.54-.322-1.43-.322-1.98 0L8.019 21.43c-2.14 1.28-3.44.322-2.88-2.117l.71-3.095c.13-.585-.1-1.402-.52-1.825l-2.48-2.5C1.39 10.42 1.86 8.929 3.899 8.586l3.19-.535c.53-.09 1.17-.564 1.41-1.058l1.76-3.549c.96-1.925 2.52-1.925 3.47 0"
+                    color="#fff"
+                  />
+                </svg>
+                {{ anime.score }} - {{ anime.type }} - {{ anime.year }} - {{ anime.status }}
+              </p>
+              <button class="flex flex-col items-center cursor-pointer">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24">
+                  <path
+                    fill="#fff"
+                    d="m11 19.654l-1.156-1.042q-2.28-2.087-3.799-3.593T3.632 12.34q-.896-1.173-1.264-2.146T2 8.225q0-1.908 1.296-3.201T6.5 3.731q1.32 0 2.475.675T11 6.369q.87-1.288 2.025-1.963T15.5 3.73q1.737 0 2.948 1.08q1.212 1.08 1.48 2.689H18.9q-.238-1.23-1.19-2t-2.21-.77q-1.217 0-2.21.688T11.403 7.5h-.804q-.91-1.413-1.907-2.091T6.5 4.73q-1.463 0-2.482.997Q3 6.725 3 8.225q0 .844.35 1.714t1.25 2t2.45 2.658T11 18.3q.766-.69 1.506-1.364q.74-.672 1.323-1.23l.11.11l.246.247l.248.247l.11.11q-.59.557-1.314 1.208t-1.471 1.341zm6.73-3.154v-3h-3v-1h3v-3h1v3h3v1h-3v3z"
+                  />
+                </svg>
+                Add to watchlist
+              </button>
+            </div>
           </div>
         </div>
       </div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,11 +3,10 @@ import Nav from '@/components/Nav.vue'
 </script>
 
 <template>
-  <div class="h-screen w-full bg-[#262626] text-center text-gray-100">
+  <div
+    class="h-screen w-full bg-[#262626] text-center text-gray-100 bg-cover bg-center"
+    style="background-image: url('/images/landscape2.jpg')"
+  >
     <Nav />
-    <!-- <div
-      class="w-full h-full bg-cover bg-center"
-      style="background-image: url('/images/landscape2.jpg')"
-    ></div> -->
   </div>
 </template>
